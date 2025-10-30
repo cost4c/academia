665337e6e288056458e98f00173bc0477752bbc3
Fred Alef Costa Fabiano <20242INN007@cefetes.br>
2025-10-29 21:01:56 -0300

    Atualização backend

--- src/db/data.js
+++ src/db/data.js
@@ -2,9 +2,7 @@
 
 export const planos = [
   { id: 1, nome: "Musculação", preco: 120, duracao: "Mensal" },
-  { id: 2, nome: "Crossfit", preco: 180, duracao: "Mensal" },
-  { id: 3, nome: "Funcional", preco: 150, duracao: "Mensal" },
-  { id: 4, nome: "Pilates", preco: 200, duracao: "Mensal" },
+  { id: 2, nome: "Funcional", preco: 150, duracao: "Mensal" },
 ];
 
 export const alunos = [];
